63a88dca7702f711abe1e49fe95892d617c1c89e
Steve Downing <steven.downing@arup.com>
2013-06-10 22:05:34 +1000

    Setup solution + numl example

---
+++ Numl_quickstart/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data;
+using numl;
+using numl.Model;
+using numl.Supervised;
+
+namespace Numl_quickstart
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Tennis[] data = Tennis.GetData();
+         var d = Descriptor.Create<Tennis>();
+         var g = new DecisionTreeGenerator(d);
+         g.SetHint(false);
+         var model = Learner.Learn(data, 0.8, 1000, g);
+
+         Console.WriteLine(model);
+         Console.ReadKey();
+
+      }
+   }
+}
